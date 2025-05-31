98a5baa0f75f47f64100e279e7611408e1a92054
Xiaoan Wang <xiaoan.sarah.wang@gmail.com>
2025-05-31 23:44:33 +0800

    Create visualization.py

---
+++ neurosuite/visualization.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_trial(trial, ch_names=None):
+    """
+    trial: (time, channels)
+    """
+    time = np.arange(trial.shape[0])
+    plt.figure(figsize=(10, 6))
+    for i in range(trial.shape[1]):
+        plt.plot(time, trial[:, i] + i * 50, label=ch_names[i] if ch_names else f"Ch {i}")
+    plt.xlabel("Time")
+    plt.title("EEG Trial")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
