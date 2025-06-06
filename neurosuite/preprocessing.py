d8179ae6d5ed61d1cad69c0fdadca2e90911d79f
Xiaoan Wang <xiaoan.sarah.wang@gmail.com>
2025-06-06 11:32:24 +0800

    Update preprocessing.py

--- neurosuite/preprocessing.py
+++ neurosuite/preprocessing.py
@@ -11,11 +11,13 @@ class Preprocessor:
         self.apply_car = config.get("apply_car", False)
         self.apply_ica = config.get("apply_ica", False)
         self.apply_baseline = config.get("apply_baseline", False)
-        self.baseline_window = config.get("baseline_window", (0, 1)) 
+        self.baseline_window = config.get("baseline_window", (0, 1))  
 
-        # Channel names and types (you can generalize this later)
-        self.ch_names = config.get("channel_names", None)
-        self.ch_types = config.get("channel_types", "eeg")
+        self.ch_names = config.get("channel_names", None)  
+        self.ch_types = config.get("channel_types", "eeg")  
+
+    def _infer_channel_names(self, n_channels):
+        return [f"EEG {i+1:03}" for i in range(n_channels)]
 
     def bandpass_filter(self, data):
         b, a = butter(4, [self.lowcut / (0.5 * self.fs), self.highcut / (0.5 * self.fs)], btype="band")
@@ -32,17 +34,20 @@ class Preprocessor:
         return data - baseline
 
     def apply_ica_mne(self, data):
-        if self.ch_names is None:
-            n_channels = data.shape[0]
-            self.ch_names = [f"EEG {i+1}" for i in range(n_channels)]
+        n_channels = data.shape[0]
+        ch_names = self.ch_names or self._infer_channel_names(n_channels)
 
-        info = mne.create_info(ch_names=self.ch_names, sfreq=self.fs, ch_types=self.ch_types)
+        if isinstance(self.ch_types, str):
+            ch_types = [self.ch_types] * n_channels
+        else:
+            ch_types = self.ch_types
+
+        info = mne.create_info(ch_names, self.fs, ch_types)
         raw = mne.io.RawArray(data, info)
         raw.filter(self.lowcut, self.highcut, fir_design='firwin', verbose=False)
 
         ica = mne.preprocessing.ICA(n_components=0.95, random_state=42, max_iter="auto", verbose=False)
         ica.fit(raw)
-        ica.detect_artifacts(raw)
         raw_clean = ica.apply(raw.copy())
         return raw_clean.get_data()
 
@@ -52,7 +57,7 @@ class Preprocessor:
             if self.apply_ica:
                 trial = self.apply_ica_mne(trial)
 
-            if self.apply_filter:
+            if self.apply_filter and not self.apply_ica: 
                 trial = np.array([self.bandpass_filter(ch) for ch in trial])
 
             if self.apply_car:
@@ -63,3 +68,4 @@ class Preprocessor:
 
             X_out.append(trial)
         return np.array(X_out)
+
